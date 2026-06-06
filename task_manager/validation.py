f92aee43dc3b438c3d3f04779853d7a7b8a29229
gabrielmutavi4-GM <gabrielmutavi4@gmail.com>
2026-06-06 16:34:57 +0300

    update project files

--- task_manager/validation.py
+++ task_manager/validation.py
@@ -1,26 +1,19 @@
 from datetime import datetime
 
 def validate_task_title(title):
-    if len(title.strip()) == 0:
+    if len(title) == 0:
         raise ValueError("Title cannot be empty")
     return True
 
 
 def validate_task_description(description):
-    if len(description.strip()) == 0:
+
+    if len(description) == 0:
         raise ValueError("Description cannot be empty")
     return True
 
 
-from datetime import datetime
-
 def validate_due_date(due_date):
-    if len(due_date.strip()) == 0:
+    if len(due_date) == 0:
         raise ValueError("Due date cannot be empty")
-
-    try:
-        datetime.strptime(due_date, "%Y-%m-%d")
-    except ValueError:
-        raise ValueError("Date must be YYYY-MM-DD")
-
     return True
